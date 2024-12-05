58a5b36326f051caf8afab77f49df3b828fde596
jayel888 <15644@coderacademy.edu.au>
2024-12-05 21:02:32 +1100

    feat: created watchlist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,53 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import MovieCard from '../components/MovieCard';
+import axios from "axios";
+import React, { useState } from "react";
+import MovieCard from "../components/MovieCard";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
 
     const fetchMovies = async () => {
-        // using fetch()
-        // const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`);
+        // Using fetch()
+        // const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`); 
 
         // Using axios()
         if (query) {
-            const response = await axios.get(`https://www.omdbapi.com/`, {
+            const response = await axios.get(`http://www.omdbapi.com/`, {
                 params: {
-                    apikey: 'babe2cdb',
+                    apikey: 'd58efa1d',
                     s: query
                 }
             });
             console.log(response.data);
             setMovies(response.data.Search || []);
         }
-    }
+    };
 
     return (
         <div className="container my-4">
 
             <h1 className="text-center mb-4">Movie Search</h1>
             <div className="row">
-                <div className="col-md-8 offset-md-2">
-                <input
-                    type="text"
-                    placeholder="Search for Movies.."
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                />
-                <button className='btn btn-primary btn-block mt-2' onClick={fetchMovies}>Search</button>
-                <div className='row mt-4'>
-                    {
-                        movies.map((movie) => (
-                            <MovieCard key={movie.imdbID} movie={movie} />
-                        ))
-                    }
+                <div className="col-md-8 offset-md-4">
+                    <input
+                        type="text"
+                        placeholder="Search for Movies..."
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                    <button className="btn btn-primary btn-block mt-2" onClick={fetchMovies}>Search</button>
                 </div>
+
+                <Link to='/watchlist'>Watchlist</Link>
             </div>
-        </div>
+            <div className="row mt-4">
+                {
+                    movies.map((movie) => (
+                        <MovieCard key={movie.imdbID} movie={movie} />
+                    ))
+                }
+            </div>
         </div>
     )
 }
