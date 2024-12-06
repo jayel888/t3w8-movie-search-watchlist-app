6c430c630b7251441880b6b3ee5ff8d736658fa5
jayel888 <15644@coderacademy.edu.au>
2024-12-07 10:59:17 +1100

    feat: Details page added, back navigation button created as well

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home.jsx';
 import WatchlistProvider from './context/WatchlistContext.jsx';
 import Watchlist from './pages/Watchlist.jsx';
 import Navbar from './components/Navbar.jsx';
+import Details from './pages/Details.jsx';
 
 function App() {
 
@@ -15,11 +16,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/watchlist' element={<Watchlist />} />
-          <Route path='/details/:id' element={<div>Details</div>} />
+          <Route path='/details/:id' element={<Details />} />
         </Routes>
       </Router>
     </WatchlistProvider>
   )
 }
 
-export default App
+export default App;
